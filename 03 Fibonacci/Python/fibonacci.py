eaf018913318d4430aaeb96eb82a57d59a54e6e7
PandorasActor0 <nelsona2001@hotmail.com>
2024-07-16 16:29:30 -0500

    add Fibonacci

---
+++ 03 Fibonacci/Python/fibonacci.py
@@ -0,0 +1,20 @@
+"""
+ * Escribe un programa que imprima los 50 primeros números de la sucesión
+ * de Fibonacci empezando en 0.
+ * - La serie Fibonacci se compone por una sucesión de números en
+ *   la que el siguiente siempre es la suma de los dos anteriores.
+ *   0, 1, 1, 2, 3, 5, 8, 13...
+"""
+
+def fibonacci():
+    a = 0
+    b = 1
+    print(a)
+    print(b)
+    for i in range(50-3):
+        c = a+b
+        print(c)
+        a = b
+        b = c
+
+fibonacci()
